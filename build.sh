b3fbb85b741ef9f64772f9a1a6ce673ac757b10d
wangxin <wangxin01@loongson.cn>
2026-03-18 10:30:28 +0800

    -fix-build-for-version-25.2
    Change-Id: I531bb36ce832df84e6e97ee1865585d7325a3e23

--- build.sh
+++ build.sh
@@ -13,11 +13,11 @@ if [ -d ./out ]; then
 fi
 
 if [ "$PACKAGEFORMAT" = rpm ]; then
-    cp ./workdir/installation/LoongOffice_languagepack/rpm/install/LoongOffice_7.3.6.2.0_Linux_rpm_langpack_zh-CN/RPMS/loongofficebasis-zh-CN-help-7.3.6.2.0-2.loongarch64.rpm \
-       ./workdir/installation/LoongOffice_languagepack/rpm/install/LoongOffice_7.3.6.2.0_Linux_rpm_langpack_zh-CN/RPMS/loongoffice-zh-CN-7.3.6.2.0-2.loongarch64.rpm \
-       ./workdir/installation/LoongOffice_languagepack/rpm/install/LoongOffice_7.3.6.2.0_Linux_rpm_langpack_zh-CN/RPMS/loongofficebasis-zh-CN-7.3.6.2.0-2.loongarch64.rpm \
-       ./workdir/installation/LoongOffice/rpm/install/LoongOffice_7.3.6.2.0_Linux_rpm/RPMS/loongoffice-7.3.6.2.0-2.loongarch64.rpm \
-       ./workdir/installation/LoongOffice/rpm/install/LoongOffice_7.3.6.2.0_Linux_rpm/RPMS/loongoffice-freedesktop-menus-7.3.6-2.noarch.rpm \
+    cp ./workdir/installation/LoongOffice_languagepack/rpm/install/LoongOffice_25.8.7.0.0_Linux_rpm_langpack_zh-CN/RPMS/loongofficebasis-zh-CN-help-25.8.7.0.0-2.loongarch64.rpm \
+       ./workdir/installation/LoongOffice_languagepack/rpm/install/LoongOffice_25.8.7.0.0_Linux_rpm_langpack_zh-CN/RPMS/loongoffice-zh-CN-25.8.7.0.0-2.loongarch64.rpm \
+       ./workdir/installation/LoongOffice_languagepack/rpm/install/LoongOffice_25.8.7.0.0_Linux_rpm_langpack_zh-CN/RPMS/loongofficebasis-zh-CN-25.8.7.0.0-2.loongarch64.rpm \
+       ./workdir/installation/LoongOffice/rpm/install/LoongOffice_25.8.7.0.0_Linux_rpm/RPMS/loongoffice-25.8.7.0.0-2.loongarch64.rpm \
+       ./workdir/installation/LoongOffice/rpm/install/LoongOffice_25.8.7.0.0_Linux_rpm/RPMS/loongoffice-freedesktop-menus-7.3.6-2.noarch.rpm \
        ./out
     exit 0
 fi
@@ -29,12 +29,12 @@ if [ "$MACHINE" = mips64 ]; then
     MACHINE=mips64el
 fi
 
-cp ../workdir/installation/LoongOffice/deb/install/LoongOffice_7.3.6.2.0_Linux_deb/DEBS/loongoffice_7.3.6.2.0-2_${MACHINE}.deb \
-   ../workdir/installation/LoongOffice/deb/install/LoongOffice_7.3.6.2.0_Linux_deb/DEBS/loongoffice-debian-menus_7.3.6-2_all.deb \
-   ../workdir/installation/LoongOffice_languagepack/deb/install/LoongOffice_7.3.6.2.0_Linux_deb_langpack_zh-CN/DEBS/loongofficebasis-zh-cn_7.3.6.2.0-2_${MACHINE}.deb \
-   ../workdir/installation/LoongOffice_languagepack/deb/install/LoongOffice_7.3.6.2.0_Linux_deb_langpack_zh-CN/DEBS/loongofficebasis-zh-cn-help_7.3.6.2.0-2_${MACHINE}.deb \
-   ../workdir/installation/LoongOffice_languagepack/deb/install/LoongOffice_7.3.6.2.0_Linux_deb_langpack_zh-CN/DEBS/loongoffice-zh-cn_7.3.6.2.0-2_${MACHINE}.deb \
-   ../workdir/installation/LoongOffice_SDK/deb/install/LoongOffice_7.3.6.2.0_Linux_deb_sdk/DEBS/loongoffice-sdk_7.3.6.2.0-2_${MACHINE}.deb .
+cp ../workdir/installation/LoongOffice/deb/install/LoongOffice_25.8.7.0.0_Linux_deb/DEBS/loongoffice_25.8.7.0.0-2_${MACHINE}.deb \
+   ../workdir/installation/LoongOffice/deb/install/LoongOffice_25.8.7.0.0_Linux_deb/DEBS/loongoffice-debian-menus_7.3.6-2_all.deb \
+   ../workdir/installation/LoongOffice_languagepack/deb/install/LoongOffice_25.8.7.0.0_Linux_deb_langpack_zh-CN/DEBS/loongofficebasis-zh-cn_25.8.7.0.0-2_${MACHINE}.deb \
+   ../workdir/installation/LoongOffice_languagepack/deb/install/LoongOffice_25.8.7.0.0_Linux_deb_langpack_zh-CN/DEBS/loongofficebasis-zh-cn-help_25.8.7.0.0-2_${MACHINE}.deb \
+   ../workdir/installation/LoongOffice_languagepack/deb/install/LoongOffice_25.8.7.0.0_Linux_deb_langpack_zh-CN/DEBS/loongoffice-zh-cn_25.8.7.0.0-2_${MACHINE}.deb \
+   ../workdir/installation/LoongOffice_SDK/deb/install/LoongOffice_25.8.7.0.0_Linux_deb_sdk/DEBS/loongoffice-sdk_25.8.7.0.0-2_${MACHINE}.deb .
 
 if [ -d ./extract ]; then
     rm -rf ./extract
@@ -47,19 +47,19 @@ fi
 mkdir -p ./extract/DEBIAN
 mkdir -p ./extract-sdk/DEBIAN
 
-dpkg-deb -x ./loongoffice-zh-cn_7.3.6.2.0-2_${MACHINE}.deb extract
-dpkg-deb -x ./loongofficebasis-zh-cn-help_7.3.6.2.0-2_${MACHINE}.deb extract
-dpkg-deb -x ./loongofficebasis-zh-cn_7.3.6.2.0-2_${MACHINE}.deb extract
+dpkg-deb -x ./loongoffice-zh-cn_25.8.7.0.0-2_${MACHINE}.deb extract
+dpkg-deb -x ./loongofficebasis-zh-cn-help_25.8.7.0.0-2_${MACHINE}.deb extract
+dpkg-deb -x ./loongofficebasis-zh-cn_25.8.7.0.0-2_${MACHINE}.deb extract
 dpkg-deb -x ./loongoffice-debian-menus_7.3.6-2_all.deb extract
-dpkg-deb -x ./loongoffice_7.3.6.2.0-2_${MACHINE}.deb extract
+dpkg-deb -x ./loongoffice_25.8.7.0.0-2_${MACHINE}.deb extract
 
 dpkg-deb -e ./loongoffice-debian-menus_7.3.6-2_all.deb extract/DEBIAN
-dpkg-deb -e ./loongoffice_7.3.6.2.0-2_${MACHINE}.deb extract/DEBIAN
+dpkg-deb -e ./loongoffice_25.8.7.0.0-2_${MACHINE}.deb extract/DEBIAN
 
-dpkg-deb -x ./loongoffice-sdk_7.3.6.2.0-2_${MACHINE}.deb extract-sdk
-dpkg-deb -e ./loongoffice-sdk_7.3.6.2.0-2_${MACHINE}.deb extract-sdk/DEBIAN
+dpkg-deb -x ./loongoffice-sdk_25.8.7.0.0-2_${MACHINE}.deb extract-sdk
+dpkg-deb -e ./loongoffice-sdk_25.8.7.0.0-2_${MACHINE}.deb extract-sdk/DEBIAN
 
-sed -i "s/7.3.6.2.0-2$/7.3.6.2.0-2.lnd.1.0.0006/g" extract/DEBIAN/control
+sed -i "s/25.8.7.0.0-2$/25.8.7.0.0-2.lnd.1.0.0006/g" extract/DEBIAN/control
 sed -i "s/.*Installed-Size.*/Installed-Size:\ `du -sk extract | awk '{print $1}'`/g" extract/DEBIAN/control
 
 if [ ! -d ./build ]; then
